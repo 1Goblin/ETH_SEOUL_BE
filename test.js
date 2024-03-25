6b3e9d74d43fa334866d445bf8ffea160b9ce7cd
1Goblin <rkdalstjd122@naver.com>
2024-03-25 20:24:28 +0900

    mongo

--- test.js
+++ test.js
@@ -1,2 +1,38 @@
-console.log("hello world");
-console.log("테스트");
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+// Mongoose connection without the deprecated options
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    connectTimeoutMS: 30000, // Increase timeout to 30 seconds
+
+    socketTimeoutMS: 45000,
+  })
+
+  .then(() => {
+    console.log("Connected to MongoDB via Mongoose");
+
+    // Middleware
+
+    app.use(express.json());
+
+    app.use("/api/auth", authRoute);
+
+    // Start the Express server
+
+    app.listen(8800, () => {
+      console.log("Backend server is running");
+    });
+  })
+
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
