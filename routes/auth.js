9aa77e6903ca402a398fae7fa2692f865f92d943
jaerius <rylynn1029@naver.com>
2024-03-28 00:49:41 +0900

    api 연결

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,11 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const CryptoJS = require("crypto-js");
-const jwt = require("jsonwebtoken")
+
 
 //REGISTER
 router.post("/register", async (req,res) => {
     const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString(),
-
+        username: req.body.username
     });
     console.log(newUser)
     try{
--- test.js
+++ test.js
@@ -1,14 +1,18 @@
 const express = require("express");
-
+const User = require("./models/User");
 const mongoose = require("mongoose");
-
+const router = require("express").Router();
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const app = express();
 
+const authRouter = require('./routes/auth') // 해당 파일에서 정의한 라우트를 객체화 함
+
+
 // Mongoose connection without the deprecated options
+
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -22,14 +26,18 @@ mongoose
     // Middleware
 
     app.use(express.json());
+    app.use('/api', authRouter) // authRouter에서 정의된 모든 경로에 자동으로 /api 붙임
 
     // Start the Express server
 
     app.listen(8800, () => {
       console.log("Backend server is running");
     });
+
+
   })
 
   .catch((err) => {
     console.error("MongoDB connection error:", err);
   });
+
